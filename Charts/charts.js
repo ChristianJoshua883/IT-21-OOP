166ebb8463d5c695b7dd6513126e6ac7bb2042ce
Krai-02 <20231874@nbsc.edu.ph>
2024-10-25 09:14:49 +0800

    added js document.getElementByID

--- Charts/charts.js
+++ Charts/charts.js
@@ -1,43 +1,36 @@
-const DATA_COUNT = 7;
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'Female',
-            data: [10, 30, 39, 20, 25, 34,],
-            borderColor: 'rgba(255, 99, 132, 1)',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        },
-        {
-            label: 'Male',
-            data: [18, 33, 22, 19, 11, 39, 30],
-            borderColor: 'rgba(54, 162, 235, 1)',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        }
-    ]
-};
-
-const config = {
-    type: 'line',
-    data: data,
+const ctx = document.getElementById('doughnut').getContext('2d');
+const doughnut = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',               
+            ],
+            borderWidth: 1
+        }]
+    },
     options: {
-        responsive: true,
-        plugins: {
-            title: {
-                display: true,
-                text: 'Male and Female Attendance Line Chart'
-            }
-        },
         scales: {
             y: {
-                suggestedMin: 30,
-                suggestedMax: 50,
+                beginAtZero: true
             }
         }
-    },
-};
+    }
+});
 
-const myChart = new Chart( document.getElementById('myChart'), config
-);
+    
