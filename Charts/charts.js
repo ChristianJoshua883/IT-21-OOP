4a132cdf6881c0c969c0e97a0bcb881e7bb820aa
Krai-02 <20231874@nbsc.edu.ph>
2024-10-25 10:15:31 +0800

    change the chart option

--- Charts/charts.js
+++ Charts/charts.js
@@ -1,37 +1,35 @@
 const CHART_LABELS = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
 const CHART_DATA = [12, 19, 3, 5, 2, 3];
 
-const chartData = {
-    labels: CHART_LABELS,
-    datasets: [
-        {
-            label: 'Votes',
-            data: CHART_DATA,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }
-    ]
-};
+    type: 'doughnut'
 
-const chartConfig = {
-    type: 'doughnut',
-    data: chartData,
-    options: {
+    const chartData = {
+        labels: CHART_LABELS,
+        datasets: [
+            {
+                label: 'My First CART',
+                data: CHART_DATA,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(153, 102, 255)',
+                    'rgb(255, 159, 64)' 
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+    };
+    const chartOptions = {
         responsive: true,
         plugins: {
             title: {
@@ -44,7 +42,12 @@ const chartConfig = {
                 beginAtZero: true
             }
         }
-    },
-};
+    };
+    const chartConfig = {
+        type: 'doughnut',
+        data: chartData,
+        options: chartOptions
+    };
 
-const doughnutChart = new Chart(document.getElementById('doughnut'), chartConfig);
+    const doughnutconst = new Chart(document.getElementById('doughnut'), chartConfig);Chart = new Chart(document.getElementById('doughnut'), chartConfig);
+    
